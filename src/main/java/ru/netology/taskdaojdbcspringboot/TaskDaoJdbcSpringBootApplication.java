152c5b7be0879239e9b5b91e62974dc25cbc412d
Anatolii Vasilev <avas2012@gmail.com>
2022-09-19 22:15:04 +0300

    first commit

---
+++ src/main/java/ru/netology/taskdaojdbcspringboot/TaskDaoJdbcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.taskdaojdbcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDaoJdbcSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskDaoJdbcSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/taskdaojdbcspringboot/TaskDaoJdbcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.taskdaojdbcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDaoJdbcSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
